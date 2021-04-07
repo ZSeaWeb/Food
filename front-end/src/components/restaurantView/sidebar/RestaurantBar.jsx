ea321127993ecc8cbb59aca0a318c62cb3d65831
Xin Liu <liu.xin7@northeastern.edu>
2021-04-06 22:44:27 -0700

    Add the icon for the toolbar.

--- front-end/src/components/restaurantView/sidebar/RestaurantBar.jsx
+++ front-end/src/components/restaurantView/sidebar/RestaurantBar.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
+import './RestaurantBar.css'
 import { Link } from "react-router-dom";
 import {
-  List,
-  ListItem,
-  ListItemText,
-  Divider, AppBar, Tabs, Tab
+  AppBar, Tabs, Tab
 }
   from '@material-ui/core';
-import {TabPanel, TabContext} from "@material-ui/lab";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import HelpIcon from '@material-ui/icons/Help';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import AddAlarmIcon from '@material-ui/icons/AddAlarm';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
 
 class RestaurantBar extends React.Component {
   render() {
     return (
       <div>
-        <br />
-        <h3><b><i>Restaurant ToolBar</i></b></h3>
-        <br />
-        <AppBar position="static">
+        <AppBar position="static" style={{ background: '#2E3B55' }}>
           <Tabs value="value" onChange={() => {}} aria-label="simple tabs example" centered>
             <Link to={"/restaurant/home"} className="link">
-              <Tab label="New Restaurant Instruction"/>
+              <Tab label="New Restaurant" icon={<HelpIcon />}/>
             </Link>
             <Link to={"/restaurant/information"} className="link">
-              <Tab label="Restaurant Information" />
+              <Tab label="Restaurant Information" icon={<RestaurantIcon />}/>
             </Link>
             <Link to={"/restaurant/menu"} className="link">
-              <Tab label="Menu"/>
+              <Tab label="Menu" icon={<MenuBookIcon />}/>
             </Link>
             <Link to={"/restaurant/order"} className="link">
-              <Tab label="My Active Orders"/>
+              <Tab label="My Active Orders" icon={<FavoriteIcon />}/>
             </Link>
             <Link to={"/restaurant/history"} className="link">
-              <Tab label="My Order History"/>
+              <Tab label="My Order History" icon={<AddAlarmIcon />}/>
             </Link>
           </Tabs>
         </AppBar>
