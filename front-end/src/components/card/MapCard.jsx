af73eff49ff9bade4906caa7780cbba687090d5b
livia <ainezling@gmail.com>
2021-04-23 14:56:38 -0700

    add mongodb and google map

--- front-end/src/components/card/MapCard.jsx
+++ front-end/src/components/card/MapCard.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import {
-  StaticGoogleMap,
-  Marker,
-  Direction
-} from 'react-static-google-map';
-
 class MapCard extends React.Component {
 
   render() {
-    return this.props.resAddress && this.props.cusAddress ? (
-      <StaticGoogleMap size="350x350" apiKey={process.env.REACT_APP_API_KEY}>
-        <Marker location={this.props.resAddress} color="green" label="S" />
-        <Marker location={this.props.cusAddress} color="red" label="E" />
-        <Direction weight="5"
-          origin={this.props.resAddress}
-          destination={this.props.cusAddress}
-        />
-      </StaticGoogleMap>
-    ) : null;
-  }
+      return <h6>this is a map</h6>
+    // return this.props.resAddress && this.props.cusAddress ? (
+    //     <img src={`https://maps.googleapis.com/maps/api/staticmap?size=400x400&zoom=11
+    //     &markers=color:blue%7Clabel:R%7C${this.props.resAddress}
+    //     &markers=color:red%7Clabel:C%7C${this.props.cusAddress}
+    //     &path=color:0x0000ff|weight:5|${this.props.resAddress}|${this.props.cusAddress}
+    //     &key=${process.env.REACT_APP_API_KEY}`} alt="map" />
+    // ) : null;
+    }
 }
 
 export default MapCard;
