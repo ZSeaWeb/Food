38dbe4b0f9b396da9029280117605a422ef7dc03
livia <ainezling@gmail.com>
2021-04-23 15:13:53 -0700

    merge codes

--- target/classes/src/components/card/MapCard.jsx
+++ target/classes/src/components/card/MapCard.jsx
@@ -4,21 +4,34 @@ import {
   Marker,
   Direction
 } from 'react-static-google-map';
-
+import GoogleApiWrapper from "./test"
 class MapCard extends React.Component {
 
   render() {
+      console.log(this.props.resAddress)
+      console.log(this.props.cusAddress)
+
     return this.props.resAddress && this.props.cusAddress ? (
-      <StaticGoogleMap size="350x350" apiKey={process.env.REACT_APP_API_KEY}>
-        <Marker location={this.props.resAddress} color="green" label="S" />
-        <Marker location={this.props.cusAddress} color="red" label="E" />
-        <Direction weight="5"
-          origin={this.props.resAddress}
-          destination={this.props.cusAddress}
-        />
-      </StaticGoogleMap>
+      // <StaticGoogleMap size="350x350" apiKey="AIzaSyBbm3n9WGdulBin6G5j9iJnH1SVeNMMPSU">
+      //   <Marker location={this.props.resAddress} color="green" label="S" />
+      //   <Marker location={this.props.cusAddress} color="red" label="E" />
+      //   <Direction weight="5"
+      //     origin={this.props.resAddress}
+      //     destination={this.props.cusAddress}
+      //   />
+      // </StaticGoogleMap>
+        <img src={`https://maps.googleapis.com/maps/api/staticmap?size=400x400&zoom=20
+        &markers=color:blue%7Clabel:S%7C${this.props.resAddress[0]},${this.props.resAddress[1]}
+        &key=AIzaSyBbm3n9WGdulBin6G5j9iJnH1SVeNMMPSU`} alt="Logo" />
     ) : null;
-  }
+
+    }
+  //     return (
+  //         // <GoogleApiWrapper size="350x350"/>
+//&markers=color:blue%7Clabel:S%7C${this.props.resAddress[0]},${this.props.resAddress[1]}
+  //
+  //
+
 }
 
 export default MapCard;
--- target/classes/src/components/restaurantView/sidebar/RestaurantBar.jsx
+++ target/classes/src/components/restaurantView/sidebar/RestaurantBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './RestaurantBar.css'
 import { Link } from "react-router-dom";
 import {
+<<<<<<< Updated upstream
   AppBar, Tabs, Tab
 }
   from '@material-ui/core';
@@ -10,11 +11,21 @@ import HelpIcon from '@material-ui/icons/Help';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
 import AddAlarmIcon from '@material-ui/icons/AddAlarm';
 import RestaurantIcon from '@material-ui/icons/Restaurant';
+=======
+  List,
+  ListItem,
+  ListItemText,
+  Divider, AppBar, Tabs, Tab
+}
+  from '@material-ui/core';
+import {TabPanel, TabContext} from "@material-ui/lab";
+>>>>>>> Stashed changes
 
 class RestaurantBar extends React.Component {
   render() {
     return (
       <div>
+<<<<<<< Updated upstream
         <AppBar position="static" style={{ background: '#2E3B55' }}>
           <Tabs value="value" onChange={() => {}} aria-label="simple tabs example" centered>
             <Link to={"/restaurant/home"} className="link">
@@ -31,6 +42,27 @@ class RestaurantBar extends React.Component {
             </Link>
             <Link to={"/restaurant/history"} className="link">
               <Tab label="My Order History" icon={<AddAlarmIcon />}/>
+=======
+        <br />
+        <h3><b><i>Restaurant ToolBar</i></b></h3>
+        <br />
+        <AppBar position="static">
+          <Tabs value="value" onChange={() => {}} aria-label="simple tabs example" centered>
+            <Link to={"/restaurant/home"} className="link">
+              <Tab label="New Restaurant Instruction"/>
+            </Link>
+            <Link to={"/restaurant/information"} className="link">
+              <Tab label="Restaurant Information" />
+            </Link>
+            <Link to={"/restaurant/menu"} className="link">
+              <Tab label="Menu"/>
+            </Link>
+            <Link to={"/restaurant/order"} className="link">
+              <Tab label="My Active Orders"/>
+            </Link>
+            <Link to={"/restaurant/history"} className="link">
+              <Tab label="My Order History"/>
+>>>>>>> Stashed changes
             </Link>
           </Tabs>
         </AppBar>
